681314b35dc0c36b87eb5a2fb777a164f09de789
Dong Liu <doli@kth.se>
2020-05-22 15:29:52 +0200

    add tracking file

---
+++ src/app/elib/elib.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elib',
+  templateUrl: './elib.component.html'
+})
+export class ElibComponent {
+  constructor() { }
+}
